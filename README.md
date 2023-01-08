2f0d6a70297982d8886b471e3f108ccc442fe924
Aidan <aidanreynolds715@gmail.com>
2023-01-08 13:11:07 -0500

    Addition of Javascript

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 ## Description
 
-This week one challenege is intended to showcase the skills learnt throughout the first week of the boot camp. It contains a web page with HTML and CSS elements. The code has been restructured and fixed to meet the acceptance criteria provided. 
 
 ## Installation
 
@@ -10,21 +9,6 @@ N/A
 
 ## Usage
 
-This challenge 1 webpage can be used for reference of certain topics of HTML and style.css. This challenge can be looked back upon for further use throughout the course as well. The following acceptance criteria has been followed as best as can be to achieve maximum success:
-
--Appications links function correctly
--Applications CSS selectors and properties organized to follow semantic structure
--Applications CSS file is properly commented.
--Application deployed at live URL
--Application loads with no errors
--Application GitHub URL submitted.
--GitHub repository contains application code.
--Application resembles screenshots provided.
--Repository has a unique name.
--Repository follows best practices for file structure.
--Repository follows best practices for naming.
--Repository contains descriptive commit messages.
--Repository contains quality README, screenshot and link for deployed application.
 
 ## Credits
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,42 @@ function GetInfo(){
   const newName = document.getElementById("cityInput");
   const cityName = document.getElementById("cityName");
   cityName.innerHTML = "--"+newName.value+"--"
+
+fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=03c9f747d082d258ab70c3e6fa82b314')
+.then(response => response.json())
+.then(data=>{
+  for(i=0;i<5;i++){
+    document.getElementById("day"+(i+1)+"Min").innerHTML = "Min:" + Number(data.list[i].main.temp_min-273.15).toFixed(1)+"°";
+  }
+  for(i=0;i<5;i++){
+    document.getElementById("day"+(i+1)+"Max").innerHTML = "Max:" + Number(data.list[i].main.temp_max-273.15).toFixed(1)+"°";
+  }
+  for(i=0;i<5;i++){
+    document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png";
+  }
+
+})
+
+.catch(err => alert("Unable to Load"))
 }
 
-fetch("https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=03c9f747d082d258ab70c3e6fa82b314")
+function DefaultScreen(){
+  document.getElementById("cityInput").defaultValue = "Adelaide";
+  GetInfo();
+}
 
+const d = new Date();
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+function CheckDay(day){
+  if(day + d.getDay() > 6){
+      return day + d.getDay() - 7;
+  }
+  else{
+      return day + d.getDay();
+  }
+}
+
+  for(i = 0; i<5; i++){
+      document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
+  }
